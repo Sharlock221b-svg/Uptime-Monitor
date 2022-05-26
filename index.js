3aaf0e4b591af2f112d01668bd2a04b9390d5f09
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-05-26 17:16:18 +0530

    parsing query string

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 /*
-* Primary File for an API
-*/
+ * Primary File for an API
+ */
 
 //Dependencies
-const http = require('http')
-const url = require('url')
-
+const http = require("http");
+const url = require("url");
 
 //Creating a server and making it response to any request
-const server = http.createServer( (req, res) => {
-    //Parsing the URL
-    const parsedUrl = url.parse(req.url, true);
+const server = http.createServer((req, res) => {
+  //Parsing the URL
+  const parsedUrl = url.parse(req.url, true);
 
-    //Getting the path
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  //Getting the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
-    //Getting the request method
-    const method = req.method.toUpperCase();
-    
-    //Returning the response
-    res.end('Hello World');
+  //Getting the request method
+  const method = req.method.toUpperCase();
 
-    //logging the path
-    console.log('Request received on Path:', trimmedPath, 'using method:', method);
+  //Getting the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  //Returning the response
+  res.end("Hello World");
+
+  //logging the path
+  console.log(
+    "Request received on Path:",
+    trimmedPath,
+    "using method:",
+    method,
+    " with query:",
+    queryStringObject
+  );
 });
 
 //Making server listen
