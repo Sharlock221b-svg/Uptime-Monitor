d27ef75f8718c6e88feabb4b5648802b2e2f8d4e
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-05-26 16:30:36 +0530

    started a server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-/*hey*/
+/*
+* Primary File for an API
+*/
+
+//Dependencies
+const http = require('http')
+
+//Creating a server and making it response to any request
+const server = http.createServer( (req, res) => res.end("Hello World!!"));
+
+//Making server listen
+server.listen(3000, () => console.log("Listining on port 3000"));
